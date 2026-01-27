517f6401432878b2984f20579791038a70766341
Sudeen7 <bsudeen@gmail.com>
2026-01-27 22:18:54 +0545

    chore: configure dependency injection for services and repositories

--- Inventory_Mgmt_System_Soln/InventoryMgmt.API/Program.cs
+++ Inventory_Mgmt_System_Soln/InventoryMgmt.API/Program.cs
@@ -1,6 +1,37 @@
+using InventoryMgmt.Application.Interfaces;
+using InventoryMgmt.Application.Services;
+using InventoryMgmt.Domain.Interfaces;
+using InventoryMgmt.Infrastructure;
+using InventoryMgmt.Infrastructure.Data;
+using InventoryMgmt.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Add DbContext with SqlServer
+builder.Services.AddDbContext<InventoryDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Register UnitOfWork
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//Register repositories
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped<ISupplierRepository,SupplierRepository>();
+builder.Services.AddScoped<IWarehouseRepository,WarehouseRepository>();
+builder.Services.AddScoped<IWarehouseProductRepository,WarehouseProductRepository>();
+builder.Services.AddScoped<IStockMovementRepository,StockMovementRepository>();
+
+//Register services
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+builder.Services.AddScoped<IProductService,ProductService>();
+builder.Services.AddScoped<ISupplierService,SupplierService>();
+builder.Services.AddScoped<IWarehouseService,WarehouseService>();
+builder.Services.AddScoped<IWarehouseProductService,WarehouseProductService>();
+builder.Services.AddScoped<IStockMovementService,StockMovementService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
